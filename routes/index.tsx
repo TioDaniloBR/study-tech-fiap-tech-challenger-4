5f66fa23a741ecdd0d2bd9b18cadc957e8df75a9
lucasdav <lucassouzadavanso@gmail.com>
2024-11-30 17:03:23 -0300

    inclusao react query

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,12 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { AppRoutes } from "./app.routes";
+
+const queryClient = new QueryClient();
 
 export function Routes() {
   return (
+    <QueryClientProvider client={queryClient}>
       <AppRoutes/>
+    </QueryClientProvider>
   );
 }
